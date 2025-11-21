d91d3631a6c35e78de1089ea14f263ef213dd2f3
Akinwale Idowu <idowuakinwale2013@gmail.com>
2025-11-21 14:56:53 +0100

    Testing CI pipeline

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Example: index.js
+// example: index.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,4 +8,3 @@ app.get('/', (req, res) => {"\n     res.send('Hello World!');\n   "});
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
